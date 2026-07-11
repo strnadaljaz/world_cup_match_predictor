f30feecb8610f2858df9444e030cf6005f773a00
strnadaljaz <strnadaljaz23@gmail.com>
2026-07-11 09:54:51 +0200

    fix of charts component for phones

--- web-interface/app/Components/Charts.tsx
+++ web-interface/app/Components/Charts.tsx
@@ -2,7 +2,7 @@
 
 const Charts = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div className="">
                 <h2 className="flex justify-center pb-5">Home win</h2>
                 <canvas id="home_win_chart"></canvas>
