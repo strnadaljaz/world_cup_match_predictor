a2e1b750462338cde59a9f6ab9d2786d5f3fe94d
strnadaljaz <strnadaljaz23@gmail.com>
2026-07-11 09:48:51 +0200

    adapted charts for phone screen sizes

--- web-interface/app/Components/Charts.tsx
+++ web-interface/app/Components/Charts.tsx
@@ -2,17 +2,17 @@
 
 const Charts = () => {
     return (
-        <div className="columns-3">
-            <div>
-                <h2>Home win</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div className="">
+                <h2 className="flex justify-center pb-5">Home win</h2>
                 <canvas id="home_win_chart"></canvas>
             </div>
             <div>
-                <h2>Draw</h2>
+                <h2 className="flex justify-center pb-5">Draw</h2>
                 <canvas id="draw_chart"></canvas>
             </div>
             <div>
-                <h2>Away win</h2>
+                <h2 className="flex justify-center pb-5">Away win</h2>
                 <canvas id="away_win_chart"></canvas>
             </div>
         </div>
