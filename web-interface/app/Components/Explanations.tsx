b3234003c65c0b544e36ddda761d39d548de66ab
strnadaljaz <strnadaljaz23@gmail.com>
2026-07-15 12:14:05 +0200

    team stats in explenation section

--- web-interface/app/Components/Explanations.tsx
+++ web-interface/app/Components/Explanations.tsx
@@ -1,16 +1,6 @@
 import { COLORS } from "../lib/colors";
 import { explanationDataType } from "../lib/types";
-
-const getFormColor = (result: Number) => {
-    switch (result) {
-        case 1:
-            return COLORS.win;
-        case 0.5:
-            return COLORS.draw;
-        default:
-            return COLORS.lose;
-    }
-};
+import TeamStats from "./TeamStats";
 
 type ExplanationsParameters = {
     explanationData: explanationDataType;
@@ -21,25 +11,9 @@ const Explanations = ({ explanationData }: ExplanationsParameters) => {
     const awayForm = explanationData.away_form ?? [];
 
     return (
-        <div className="grid grid-cols-2">
-            <div className="flex justify-center">
-                <div className="grid grid-cols-5 gap-1 w-50 items-center">
-                    {
-                        homeForm.map((result, index) => (
-                            <div key={index} className="w-6 h-6 rounded-md" style={{backgroundColor: getFormColor(result)}}></div> 
-                        )) 
-                    }
-                </div>
-            </div>
-            <div className="flex justify-center">
-                <div className="grid grid-cols-5 gap-1 w-50">
-                    {
-                        awayForm.map((result, index) => (
-                            <div key={index} className="w-6 h-6 rounded-md" style={{backgroundColor: getFormColor(result)}}></div> 
-                        )) 
-                    }
-                </div>
-            </div>
+        <div className="grid grid-cols-1 gap-6 rounded-2xl border border-white/10 bg-white/5 p-6 backdrop-blur-xl md:grid-cols-2">
+            <TeamStats teamName="Home Team" form={homeForm} goalDiff={explanationData.home_goal_diff} fifaRank={explanationData.home_fifa_rank} elo={explanationData.home_elo}/>           
+            <TeamStats teamName="Away Team" form={awayForm} goalDiff={explanationData.away_goal_diff} fifaRank={explanationData.away_fifa_rank} elo={explanationData.away_elo}/>           
         </div>
     );
 };
